93b1ba8ab3ba20136cbce408a2af88bf7617a236
Olga Botvinnik <olga.botvinnik@gmail.com>
2012-10-13 15:17:31 -0700

    debug printing works

---
+++ src/Week.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+class Week(object):
+	"""
+	docstring for Week
+
+	Created by Olga Botvinnik on 10/09/12 11:32:01
+	"""
+	def __init__(self, arg):
+		super(Week, self).__init__()
+		self.arg = arg
